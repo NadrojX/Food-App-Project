d6736fe1827939f8139b30c21032fd5c8f39c1c6
GALATOLA Jordan <jordan.galatola@etu.univ-amu.fr>
2022-02-04 23:29:44 +0100

    Ajout de methode pour lire les json.

--- src/main/java/app/foodapp/model/JsonGestion.java
+++ src/main/java/app/foodapp/model/JsonGestion.java
@@ -5,16 +5,16 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
+import java.util.Iterator;
 
 public class JsonGestion {
 
     private static int i = 0;
 
-    public static void jsonRead() throws FileNotFoundException {
+    public static void jsonArrayRead(String section){
         JSONParser jsonParser = new JSONParser();
         try(Reader reader = new FileReader("C:\\Users\\galat\\Desktop\\fichiers json\\findByIngredients.json")) {
             JSONArray jsonArray = (JSONArray) jsonParser.parse(reader);
@@ -23,7 +23,7 @@ public class JsonGestion {
                     return;
                 }
                 JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-                System.out.println(jsonObject.get("title"));
+                System.out.println(jsonObject.get(section));
                 i++;
             }
         } catch (IOException | ParseException e) {
@@ -31,8 +31,30 @@ public class JsonGestion {
         }
     }
 
-    public static void main(String[] args) throws FileNotFoundException {
-        jsonRead();
+    public static void jsonObjectRead(String section){
+        JSONParser jsonParser = new JSONParser();
+        try(Reader reader = new FileReader("C:\\Users\\galat\\Desktop\\fichiers json\\recipeInformation.json")){
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(reader);
+            System.out.println(jsonObject.get(section));
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void jsonObjectAndArrayRead(String sectionArray, int idPosition, String section){
+        JSONParser jsonParser = new JSONParser();
+        try(Reader reader = new FileReader("C:\\Users\\galat\\Desktop\\fichiers json\\recipeInformation.json")){
+            JSONObject jsonObjectFile = (JSONObject) jsonParser.parse(reader);
+            JSONArray jsonArray = (JSONArray) jsonObjectFile.get(sectionArray);
+            JSONObject jsonObject = (JSONObject) jsonArray.get(idPosition);
+            System.out.println(jsonObject.get(section));
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
     }
 
     public static void jsonWrite(){
