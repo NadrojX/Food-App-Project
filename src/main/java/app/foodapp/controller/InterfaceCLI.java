aa0a47aa60b785667a0339033189c3002dcbe3c8
GALATOLA Jordan <jordan.galatola@etu.univ-amu.fr>
2022-02-12 21:50:33 +0100

    Rajout de code pour la méthode receiptsResearch dans la classe InterfaceCLI. Et modification du build.gradle sourceCompatibility en 14 pour les switch.

--- src/main/java/app/foodapp/controller/InterfaceCLI.java
+++ src/main/java/app/foodapp/controller/InterfaceCLI.java
@@ -15,18 +15,15 @@ public class InterfaceCLI {
         System.out.println("\t2. Recette en favoris.\n");
         System.out.println("\t3.     Quitter.       ");
         System.out.println("-----------------------------------");
+
         Scanner scanner = new Scanner(System.in);
-        switch(scanner.nextInt()){
-            default: break;
-            case 1:
-                receiptsResearch();
-                break;
-            case 2:
-                favoriteReceipts();
-                break;
-            case 3:
-                System.exit(0);
-                break;
+
+        switch (scanner.nextInt()) {
+            default -> {
+            }
+            case 1 -> receiptsResearch();
+            case 2 -> favoriteReceipts();
+            case 3 -> System.exit(0);
         }
     }
 
@@ -34,21 +31,49 @@ public class InterfaceCLI {
         Scanner scannerIngredients = new Scanner(System.in);
         System.out.println("Quel est où qu'elles sont les ingrédients que vous possédez ? (Exemple du format d'écriture : riz, carotte en anglais)");
         String ingredients = scannerIngredients.nextLine();
+
         Scanner scannerNumber = new Scanner(System.in);
         System.out.println("Combien de recettes voulez-vous afficher ?");
+
         int number = scannerNumber.nextInt();
+
         URL url = new URL("https://api.spoonacular.com/recipes/findByIngredients?ingredients=" + ingredients + "&number=" + number + "&apiKey=a838ed2668eb4c62be56c24234c05a5c");
         URLConnection spoonacular = url.openConnection();
+
         JsonGestion.jsonArrayRead("title", spoonacular);
         System.out.println("De quelle recette ci-dessus, souhaitez-vous connaître la recette ?");
         Scanner scannerNumberArray = new Scanner(System.in);
+
         int numberArray = scannerNumberArray.nextInt();
+
         URLConnection spoonacular2 = url.openConnection();
         System.out.println("Ingrédients utilisés :");
         JsonGestion.jsonReadIngredients("usedIngredients", numberArray-1, "original", spoonacular2);
+
         URLConnection spoonacular3 = url.openConnection();
         System.out.println("Ingrédients manquants :");
         JsonGestion.jsonReadIngredients("missedIngredients", numberArray-1, "original", spoonacular3);
+
+        System.out.println("Voulez-vous ajouter cette recette au favoris ?  (Y or N ou OUI ou NON)");
+        Scanner scannerFav = new Scanner(System.in);
+        switch (scannerFav.next()) {
+            default -> {
+            }
+            case "y", "Y", "oui", "OUI" -> System.out.println("Recette ajoutée au favoris.");
+            case "n", "N", "non", "NON" -> System.out.println("Recette pas ajoutée au favoris.");
+        }
+
+        Scanner scannerEnd = new Scanner(System.in);
+        System.out.println("Que voulez-vous faire maintenant ?");
+        System.out.println("1. Retourner au menu.");
+        System.out.println("2. Quitter.");
+        switch (scannerEnd.nextInt()) {
+            default -> {
+            }
+            case 1 -> FoodAppCLIMenu();
+            case 2 -> System.exit(0);
+        }
+
     }
 
     public void favoriteReceipts(){
