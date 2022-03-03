0368ec836c8f0a94bebebe60560ed2763938ab1b
GALATOLA Jordan <jordan.galatola@etu.univ-amu.fr>
2022-03-03 17:21:41 +0100

    Ajout d'un logo. (peut-être temporaire)

--- src/main/java/app/foodapp/FoodApp.java
+++ src/main/java/app/foodapp/FoodApp.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 
@@ -14,6 +15,7 @@ public class FoodApp extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/app/foodapp/view/foodapp.fxml"));
         primaryStage.setTitle("Fun'Eat");
         primaryStage.setScene(new Scene(root));
+        primaryStage.getIcons().add(new Image("/app/foodapp/view/foodAppIco.png"));
         primaryStage.show();
     }
 
