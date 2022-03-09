12cc9845afdc23cd4f7bd4afe0a36e0203bba487
GALATOLA Jordan <jordan.galatola@etu.univ-amu.fr>
2022-03-09 15:05:00 +0100

    Refactoring du code.

--- src/main/java/app/foodapp/FoodApp.java
+++ src/main/java/app/foodapp/FoodApp.java
@@ -9,18 +9,20 @@ import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.util.Objects;
+
 
 public class FoodApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/app/foodapp/view/foodapp.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/app/foodapp/view/foodapp.fxml")));
         Scene scene = new Scene(root);
         scene.setFill(Color.TRANSPARENT);
         primaryStage.setTitle("Fun'Eat");
         primaryStage.setScene(scene);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
-        primaryStage.getIcons().add(new Image("/app/foodapp/view/foodAppIco.png"));
+        primaryStage.getIcons().add(new Image("/app/foodapp/view/images/foodAppIco.png"));
         primaryStage.show();
     }
 
